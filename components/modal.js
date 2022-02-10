83c7723799487996b80df4511d3c731a7531e90e
fermelli <luisfernandosalgadomiguez@gmail.com>
2022-02-09 20:20:07 -0400

    componente modal

---
+++ components/modal.js
@@ -0,0 +1,26 @@
+const Modal = {
+	name: 'Modal',
+	props: {
+		isOpen: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	template: /*html*/ `
+        <div v-if="isOpen" class="absolute top-0 left-0 z-10 grid place-content-center w-full h-screen bg-[#00000033]">
+            <div class="w-[500px] bg-white rounded">
+                <div class="p-4 border-b border-gray-600">
+                    <h3 class="text-xl">Modal</h3>
+                </div>
+                <div class="p-4">
+                    <p class="text-sm">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. In ab dolorum ex adipisci nulla iusto aut, temporibus amet itaque officiis, harum repudiandae optio molestiae mollitia error laudantium architecto fugiat rerum.
+                    </p>
+                </div>
+                <div class="flex justify-end p-4 border-t border-gray-600">
+                    <button @click="$emit('toggleModal')" class="px-4 py-2 rounded bg-rose-600 text-white">Close</button>
+                </div>
+            </div>
+        </div>
+    `,
+};
--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const App = {
 	name: 'App',
-	template: /*html*/ `<div>{{ message }}</div>`,
+	template: /*html*/ `
+        <div class="container mx-auto h-screen p-4">
+            <button @click="isOpenModal = !isOpenModal" class="px-4 py-2 rounded bg-emerald-500 text-white">Open</button>
+            <modal :is-open="isOpenModal" @toggle-modal="isOpenModal = !isOpenModal"/>
+        </div>
+    `,
 	data() {
 		return {
-			message: 'Hello world!',
+			isOpenModal: false,
 		};
 	},
 };
 
 const app = Vue.createApp(App);
+
+app.component('modal', Modal);
 
 app.mount('#app');
